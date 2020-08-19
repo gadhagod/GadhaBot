8c92b5f2f7c6a4c94be905881305c21668aa24d0
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-08-19 12:45:27 -0700

    Add !news

--- main.py
+++ main.py
@@ -4,35 +4,39 @@ import messages
 import os
 from webapp import keep_running
 from timestamp import last_update
+from news import headlines
 
 
 varlastupdate = last_update()
 varlastupdate = str(varlastupdate)
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print("Connected to server: " + varlastupdate)
+  async def on_ready(self):
+      print("Connected to server: " + varlastupdate)
 
-    async def on_message(self, message):
-        if message.content == '!commands' or (message.content).lower() == '!gadhacommands':
-          await message.channel.send(messages.commands().format(message))
+  async def on_message(self, message):
+      if message.content == '!commands' or (message.content).lower() == '!gadhacommands':
+        await message.channel.send(messages.commands().format(message))
 
-        if message.content == '!help' or (message.content).lower() == '!gadhahelp':
-          await message.channel.send(messages.help().format(message))
+      if message.content == '!help' or (message.content).lower() == '!gadhahelp':
+        await message.channel.send(messages.help().format(message))
 
-        if message.content == '!invite':
-            await message.channel.send(messages.invite().format(message)
-            )
+      if message.content == '!invite':
+          await message.channel.send(messages.invite().format(message)
+          )
 
-        if message.content == '!lastUpdated':
-            await message.channel.send(("Last updated: " + varlastupdate).format(message))
+      if message.content == '!lastUpdated':
+          await message.channel.send(("Last updated: " + varlastupdate).format(message))
 
-        if message.content == '!covidCases':
-            covidobj = messages.covid()
-            await message.channel.send(covidobj.cases().format(message))
-        if message.content == '!covidDeaths':
-            covidobj = messages.covid()
-            await message.channel.send(covidobj.deaths().format(message))
+      if message.content == '!covidCases':
+          covidobj = messages.covid()
+          await message.channel.send(covidobj.cases().format(message))
+      if message.content == '!covidDeaths':
+          covidobj = messages.covid()
+          await message.channel.send((covidobj.deaths()).format(message))
+
+      if message.content == '!news' or message.content == '!headlines':
+          await message.channel.send(str(headlines()).format(message))
 
 client = MyClient()
 keep_running()
