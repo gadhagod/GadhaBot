d822e22569b0a18f7c67eff6ac531aae98b09303
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-08-23 22:37:51 -0700

    Make links clickable

--- messages.py
+++ messages.py
@@ -1,14 +1,12 @@
-                       
-
 import covidbackend
 
 def help():
   print("Help query")
-  return "Hi {0.author.mention}! Thanks for using GadhaBot, by GadhaGod Electro$
+  return "Hi {0.author.mention}! Thanks for using GadhaBot, by GadhaGod Electronics! I can do a lot of miscellaneous things. Type !commands to see what all I can do.\n\nThe code for this bot is open-source. View it here: https://github.com/gadhagod/GadhaBot.\n\nGadhaGod Electronics' webstie: http://gadhagod.repl.co/"
 
 def invite():
   print("Invite query")
-  return "Click this link to invite this bot: https://discord.com/oauth2/author$
+  return "Click this link to invite this bot: https://discord.com/oauth2/authorize?client_id=714911868455747629&permissions=0&scope=bot"
 
 class covid():
   def __init__(self):
