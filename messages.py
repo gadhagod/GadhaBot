a1418d8a7c58901c999c56be547889149ceee756
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-08-23 17:40:51 -0700

    Add GadhaGod Electronics Website

--- messages.py
+++ messages.py
@@ -1,18 +1,24 @@
+                       
+
 import covidbackend
 
 def help():
   print("Help query")
-  return "Hi {0.author.mention}!, Thanks for using GadhaBot! It can do a lot of miscellaneous things and is still development. Type !commands to see what all you can do with me!\nThe code for this bot is open source. View it here: github.com/gadhagod/GadhaBot"
+  return "Hi {0.author.mention}! Thanks for using GadhaBot, by GadhaGod Electro$
 
 def invite():
   print("Invite query")
-  return "Click this link to invite this bot: https://discord.com/oauth2/authorize?client_id=714911868455747629&permissions=0&scope=bot"
+  return "Click this link to invite this bot: https://discord.com/oauth2/author$
 
 class covid():
   def __init__(self):
     self.varcases, self.vardeaths = covidbackend.data_return()
 
   def cases(self):
+    print("Covid Cases query")
+    return self.varcases + " global cases"
+
+  def deaths(self):
     print("Covid Cases query")
     return self.varcases + " global cases"
 
@@ -25,4 +31,3 @@ def commands():
   commandslist=open("commands.txt", "r")
   commandslist = commandslist.read()
   return commandslist
-
