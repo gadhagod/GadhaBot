cd88deca2a2ce4b0d9c3386d8777ff639d1a8f99
GadhaGod Electronics <69025547+gadhagod@users.noreply.github.com>
2020-09-07 12:15:48 -0700

    Change issue link

--- issues.py
+++ issues.py
@@ -12,9 +12,9 @@ def issuecreate(user, inbody):
 	repo = g.get_repo("GadhaGod/GadhaBot")
 	i = repo.create_issue(
 		title='Issue created by discord user ' + user,
-		body = ('From discord user' + user + ':\n' + inbody),
+		body = ('From discord user ' + user + ':\n' + inbody),
 		labels=[
 		repo.get_label("from discord user")
 		]
 	)
-	return 'Issue reported. Check https://github.com/gadhagod/gadhaBot/issues to see all issues.'
+	return 'Issue reported. Check https://github.com/gadhagod/GadhaBot/issues?q=is%3Aissue+is%3Aall to see all issues.'
