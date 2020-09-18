80150f696c7e5f2143cda8f4ad95d38614752f55
GadhaGod Electronics <69025547+gadhagod@users.noreply.github.com>
2020-09-18 10:43:26 -0700

    Create city.py

---
+++ src/city.py
@@ -0,0 +1,27 @@
+import os
+from rockset import Client, Q, F
+
+collectionName = 'GadhaBotUsers'
+rs = Client(api_key = os.environ.get('ROCKSET_SECRET'), api_server="api.rs2.usw2.rockset.com")
+collection = rs.Collection.retrieve(collectionName)
+                        
+def store(userid, city):
+	print('City store query')	
+	docs = {
+		'id': userid, 
+		'city': city
+	}
+	collection.add_docs([docs])
+	return('City stored. Type !weather to see weather in your city.')
+	
+def get(userid):
+	struserid = str(userid)
+	res = rs.sql(Q(collectionName).where(F['_id'] == struserid).select(F['city']))
+	cityobj = res[0]
+	city = cityobj['city']
+	return city
+	
+	
+def rm(userid):
+	struserid = str(userid)
+	ret = collection.remove_docs([userid])
