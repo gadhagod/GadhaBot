09d5e389412f3d67ca57f6b81b83377ed6ff927c
GadhaGod Electronics <69025547+gadhagod@users.noreply.github.com>
2020-09-18 10:52:19 -0700

    Retrieve and store cities from rockset

--- src/weather.py
+++ src/weather.py
@@ -1,37 +1,44 @@
 import requests
 import os
+import city as citydb
 
 def CtoF(C):
     F = (C * (9/5)) + 32
     F = round(F, 2)
     return F
 
-def weather(city):
+def findweather(city):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city, os.environ.get('WEATHER_SECRET'))
+    req = requests.get(url)
+    print(req)
+    info = req.json()
+
+    Ctemp = info['main']['temp']
+    Cfeels_like = info['main']['feels_like']
+    humidity = info['main']['humidity']
+    description = info['weather'][0]['description']
+    Cmintemp = info['main']['temp_min']
+    Cmaxtemp = info['main']['temp_max']
+
+    Ftemp = CtoF(float(Ctemp))
+    Ffeels_like = CtoF(float(Cfeels_like))
+    Fmintemp = CtoF(float(Cmintemp))
+    Fmaxtemp = CtoF(float(Cmaxtemp))
+
+    strdescription = 'Status: ' + str(description) + '\n'
+    strtemp = 'Current temperature: ' + str(Ctemp) + '°C, ' + str(Ftemp) + '°F\n'
+    strfeels_like = 'Feels like: ' + str(Cfeels_like) + '°C, ' + str(Ffeels_like) + '°F\n'
+    strmaxtemp = 'Today\'s high: ' + str(Cmaxtemp) + '°C, ' + str(Fmaxtemp) + '°F\n'
+    strmintemp = 'Today\'s low: ' + str(Cmintemp) + '°C, ' + str(Fmintemp) + '°F\n'
+    strhumidity = 'Humidity: ' + str(humidity) + '%'
+
+    return strdescription, strtemp, strfeels_like, strmaxtemp, strmintemp, strhumidity
+
+
+def weather(userid):
     print('Weather query')
+    city = citydb.get(userid)
     if city == '':
-        return 'Please specify city.'
+        return(False)
     else:
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city, os.environ.get('WEATHER_SECRET'))
-        req = requests.get(url)
-        data = req.json()
-
-        Ctemp = data['main']['temp']
-        Cfeels_like = data['main']['feels_like']
-        humidity = data['main']['humidity']
-        description = data['weather'][0]['description']
-        Cmintemp = data['main']['temp_min']
-        Cmaxtemp = data['main']['temp_max']
-
-        Ftemp = CtoF(float(Ctemp))
-        Ffeels_like = CtoF(float(Cfeels_like))
-        Fmintemp = CtoF(float(Cmintemp))
-        Fmaxtemp = CtoF(float(Cmaxtemp))
-
-        strdescription = 'Status: ' + str(description) + '\n'
-        strtemp = 'Current temperature: ' + str(Ctemp) + '°C, ' + str(Ftemp) + '°F\n'
-        strfeels_like = 'Feels like: ' + str(Cfeels_like) + '°C, ' + str(Ffeels_like) + '°F\n'
-        strmaxtemp = 'Today\'s high: ' + str(Cmaxtemp) + '°C, ' + str(Fmaxtemp) + '°F\n'
-        strmintemp = 'Today\'s low: ' + str(Cmintemp) + '°C, ' + str(Fmintemp) + '°F\n'
-        strhumidity = 'Humidity: ' + str(humidity) + '%'
-
-        return strdescription, strtemp, strfeels_like, strmaxtemp, strmintemp, strhumidity
+        return(findweather(city))
