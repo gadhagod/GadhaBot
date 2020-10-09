83b8836f1950be596f8e917cabcd2e281683bb53
Ubuntu <gadhagod.noreply@gmail.com>
2020-10-09 19:04:14 +0000

    Use API for covid data

--- src/corona.py
+++ src/corona.py
@@ -1,27 +1,4 @@
-import requests
-
-def connect():
-    html_code = requests.get("https://www.worldometers.info/coronavirus/").text[:100001]
-    html_code_line = html_code.split('\n')
-    return html_code_line
-
-
-class global_data:
-    def __init__(self, code):
-        self.code = code
-
-    def cases(self):
-        cases = self.code[self.code.index('<h1>Coronavirus Cases:</h1>') + 2]
-        cases = cases.replace('<span style="color:#aaa">', '')
-        cases = cases.replace('</span>', '')
-        return cases
-
-    def deaths(self):
-        deaths = self.code[self.code.index('<h1>Deaths:</h1>') + 2]
-        deaths = deaths.replace('<span>', '')
-        deaths = deaths.replace('</span>', '')
-        return deaths
+from SimpleCovidAPI import sca
 
 def data_return():
-    connected = global_data(connect())
-    return connected.cases(), connected.deaths()
+    return sca.cases(), sca.deaths()
